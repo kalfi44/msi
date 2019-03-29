4a964ea0879c06019d17d7a2b5835d5f04561036
jkalfas <jkalfas179@gmail.com>
2019-03-29 11:24:23 +0100

    nowy tekst

--- bot.py
+++ bot.py
@@ -1,31 +1,28 @@
-# coding: utf-8
-
-# # Meet Robo: your friend
-
+# kodowanie: utf-8
 import nltk
 import warnings
 warnings.filterwarnings("ignore")
 
-#nltk.download() # for downloading packages
-
 import numpy as np
 import random
-import string # to process standard python strings
+import string 
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 f=open('chatbot.txt','r',errors = 'ignore')
-raw=f.read()
-raw=raw.lower()# converts to lowercase
-#nltk.download('punkt') # first-time use only
-#nltk.download('wordnet') # first-time use only
-sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
-word_tokens = nltk.word_tokenize(raw)# converts to list of words
+inputtxt=f.read()
+inputtxt=inputtxt.lower()# konweruj do malych liter
+#nltk.download('punkt') 
+#nltk.download('wordnet') 
+s_tokens = nltk.sent_tokenize(inputtxt)# stworz liste zdan 
+w_tokens = nltk.word_tokenize(inputtxt)# stworz liste slow
 
 
-sent_tokens[:2]
+s_tokens[:2]
 
-word_tokens[:5]
-
+w_tokens[:5]
+#this part does normalization
 lemmer = nltk.stem.WordNetLemmatizer()
 def LemTokens(tokens):
     return [lemmer.lemmatize(token) for token in tokens]
@@ -33,27 +30,23 @@ remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
 def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
-GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_INPUTS = ("cześć", "hej", "hello", "hi", "greetings", "sup", "what's up","hey",)
 GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
 
 # Checking for greetings
-def greeting(sentence):
+def greeting(expr):
     """If user's input is a greeting, return a greeting response"""
-    for word in sentence.split():
-        if word.lower() in GREETING_INPUTS:
+    for x in expr.split():
+        if x.lower() in GREETING_INPUTS:
             return random.choice(GREETING_RESPONSES)
-
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
 
 # Generating response
 def response(user_response):
     robo_response=''
-    sent_tokens.append(user_response)
+    s_tokens.append(user_response)
     TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
-    tfidf = TfidfVec.fit_transform(sent_tokens)
+    tfidf = TfidfVec.fit_transform(s_tokens)
     vals = cosine_similarity(tfidf[-1], tfidf)
     idx=vals.argsort()[0][-2]
     flat = vals.flatten()
@@ -63,7 +56,7 @@ def response(user_response):
         robo_response=robo_response+"I am sorry! I don't understand you"
         return robo_response
     else:
-        robo_response = robo_response+sent_tokens[idx]
+        robo_response = robo_response+s_tokens[idx]
         return robo_response
 
 
@@ -83,7 +76,7 @@ while(flag==True):
             else:
                 print("ROBO: ",end="")
                 print(response(user_response))
-                sent_tokens.remove(user_response)
+                s_tokens.remove(user_response)
     else:
         flag=False
         print("ROBO: Bye! take care..")    
